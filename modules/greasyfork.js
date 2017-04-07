fd589a66fd69141725317a13090aff7c3e3df760
Tithen-Firion <tithen.firion.0@gmail.com>
2017-04-07 17:29:24 +0200

    [greasyfork] add module

---
+++ modules/greasyfork.js
@@ -0,0 +1,19 @@
+var greasyfork = {
+  name: "Greasy Fork",
+  loadType: "document",
+  baseURL: "https://greasyfork.org",
+  loadURL: "/forum/",
+  loginURL: "/users/sign_in",
+  messages: ["New notification.", "New inbox message."],
+  URLs: ["/forum/profile/notifications", "/forum/messages/all"],
+  classes: ["Notifications", "Inbox"],
+  process: function() {
+    this.classes.forEach(function(class_, i) {
+      if(this.document.querySelector(".Sp" + class_ + "+em+span"))
+        this.notify(i);
+    });
+  },
+  loggedIn: function() {
+    return !this.document.querySelector(".SignInPopup");
+  }
+};
