822f1954f1f3020620ecfec77ae0abffd7a2eb9f
Tithen-Firion <tithen.firion.0@gmail.com>
2018-02-21 00:06:14 +0100

    [instagram] fix module

--- modules/instagram.js
+++ modules/instagram.js
@@ -27,18 +27,19 @@ var instagram = {
   process: function() {
     var countArray = Array(6).fill(0),
     timestamp = this.load("timestamp") || 0;
-    this.json.activityFeed.stories.forEach(function(story) {
+    this.json.graphql.user.activity_feed.edge_web_activity_feed.edges.forEach(function(edge) {
+      var story = edge.node;
       if(story.timestamp > timestamp)
         if(story.type > 4)
           countArray[3]++; // unknown
         else
           countArray[story.type-1]++;
     });
-    countArray[5] = this.json.followRequests.length;
+    countArray[5] = this.json.graphql.user.edge_follow_requests.edges.length;
     countArray.forEach(function(count, index) {
       if(count > 0)
         this.notify(index);
     }, this);
-    this.save("timestamp", this.json.timestamp);
+    this.save("timestamp", this.json.graphql.user.activity_feed.timestamp);
   }
 };
